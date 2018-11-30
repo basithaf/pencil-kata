fc5b4506333aba748430a35db91c2abb3769df4f
bfahumy <basithaf@gmail.com>
2018-11-30 17:38:16 -0500

    Adjust durability when writing (treats all chars equally)

--- Pencils/Pencils/Models/Pencil.cs
+++ Pencils/Pencils/Models/Pencil.cs
@@ -8,9 +8,31 @@ namespace Pencils.Models
 {
     public class Pencil
     {
+        private const uint DEFAULT_MAX_DURABILITY = 10000;
+
+        // The number of characters that can be written with a sharp point
+        private uint MaxPointDurability { get; }
+
+        // lowercase characters use 1 durability, uppercase use 2
+        public uint PointDurability { get; private set; }
+
+        
+        
+        public Pencil(uint maxDurability = DEFAULT_MAX_DURABILITY)
+        {
+            MaxPointDurability = PointDurability = maxDurability;
+            
+        }
+
         public void WriteToPage(string toWrite, Page page)
         {
-            page.Contents += toWrite;
+            foreach (char c in toWrite)
+            {
+                PointDurability--;
+                page.Contents += c;
+            }
+
+            
         }
     }
 }
--- Pencils/UnitTests/TestPencil.cs
+++ Pencils/UnitTests/TestPencil.cs
@@ -7,13 +7,43 @@ namespace UnitTests
     [TestClass]
     public class TestPencil
     {
+        Page testPage;
+        Pencil testPencil;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            testPage = new Page();
+            testPencil = new Pencil();
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            testPage = null;
+            testPencil = null;
+        }
+
         [TestMethod]
         public void TestPencilWriteToPage()
         {
-            var page = new Page();
-            var pencil = new Pencil();
-            pencil.WriteToPage("lorem ipsum", page);
-            Assert.AreEqual("lorem ipsum", page.Contents);
+            
+            testPencil.WriteToPage("lorem ipsum", testPage);
+            Assert.AreEqual("lorem ipsum", testPage.Contents);
+            testPencil.WriteToPage(" blah blah blah...", testPage);
+            Assert.AreEqual("lorem ipsum blah blah blah...", testPage.Contents);
+        }
+
+        [TestMethod]
+        public void TestPencilDurability()
+        {
+            // Should use 4 durability
+            testPencil.WriteToPage("four", testPage);
+            Assert.AreEqual<uint>(9996, testPencil.PointDurability);
+
+            // Should also use 4 durability
+            //testPencil.WriteToPage(" four ", testPage);
+            //Assert.AreEqual(9992, testPencil.PointDurability);
         }
     }
 }
