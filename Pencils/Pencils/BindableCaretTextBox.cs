b2903ca39b7ba2971c89da6d8277ee9b76ad71f5
bfahumy <basithaf@gmail.com>
2018-12-03 16:54:33 -0500

    Implemented a bindable textbox so that the edit location can be provided by clicking in the page text box

---
+++ Pencils/Pencils/BindableCaretTextBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Pencils
+{
+    /// <summary>
+    /// A textbox that allows for binding to the CaretIndex
+    /// </summary>
+    public class BindableCaretTextBox : TextBox
+    {
+        public int BindableCaretIndex {
+            get { return (int)GetValue(BindableCaretIndexProperty); }
+            set { SetValue(BindableCaretIndexProperty, value); }
+        }
+
+        public static readonly DependencyProperty BindableCaretIndexProperty =
+            DependencyProperty.Register("BindableCaretIndex", 
+                typeof(int), typeof(BindableCaretTextBox),
+                new PropertyMetadata(OnBindableCaretIndexChanged));
+
+        /// <summary>
+        /// Updated via binding, so update CaretIndex
+        /// </summary>
+        private static void OnBindableCaretIndexChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (obj is BindableCaretTextBox tb)
+            {
+                tb.CaretIndex = tb.BindableCaretIndex;
+            }
+        }
+
+        public BindableCaretTextBox() : base()
+        {
+            SelectionChanged += UpdateBindableCaretIndex;
+        }
+
+        /// <summary>
+        /// CaretIndex was updated, so BindableCaretIndex should reflect that
+        /// </summary>
+        private void UpdateBindableCaretIndex(object sender, RoutedEventArgs e)
+        {
+            BindableCaretIndex = CaretIndex;
+        }
+    }
+}
